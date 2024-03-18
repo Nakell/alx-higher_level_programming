33b917c8d3c621ccdb0084464a990eb07a100940
Nakell <mwongelijudym@gmail.com>
2024-03-18 14:43:25 -0700

    more returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sent_len = len(sentence)
+
+    if sentence:
+        first_letter = sentence[0]
+    else:
+        first_letter = None
+    return(sent_len, first_letter)
