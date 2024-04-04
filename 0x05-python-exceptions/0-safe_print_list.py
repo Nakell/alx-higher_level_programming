367663043b66253ef315faa25a9cae95422896ec
Nakell <mwongelijudym@gmail.com>
2024-04-03 22:43:48 -0700

    safe list pirnting

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if my_list == 0:
+        my_list = []
+    result = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            result +=1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return result
