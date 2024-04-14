e5766f87bba93a632b0da6a4aaad4502c0351eee
Nakell <mwongelijudym@gmail.com>
2024-04-14 08:35:09 -0700

    lists of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elem in row:
-            print("{:d}".format(elem), end="" if elem != row[-1] else "")
+            print("{:d}".format(elem), end=" " if elem != row[-1] else "")
         print()
